9ba20d51011f21e1938b4ba306f07708742b0d26
S4B6 <maroqi67@gmail.com>
2025-04-28 08:10:33 +0200

    28 apr

--- SPPdjango/settings.py
+++ SPPdjango/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'market_data',               # Name of your database
-        'USER': 'maroqi67_user',              # Username in your PostgreSQL setup
-        'PASSWORD': '9137',     # Replace with the actual password for maroqi67_user
+        'USER': 'S4B6',              # Username in your PostgreSQL setup
+        'PASSWORD': '11sb99',     # Replace with the actual password for maroqi67_user
         'HOST': 'localhost',                  # Typically localhost if it’s on your machine
         'PORT': '5432',                  # Default PostgreSQL port
     }
